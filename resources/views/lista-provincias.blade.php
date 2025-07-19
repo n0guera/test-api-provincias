0eb130692ab47882957dfb7b81ca4bcda7e20335
Santiago Noguera <santiiago.noguera@gmail.com>
2025-07-19 00:04:55 -0300

    Add lista-provincias view

--- resources/views/lista-provincias.blade.php
+++ resources/views/lista-provincias.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Provincias de Argentina</title>
+    <!--Style/Scripts-->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     
