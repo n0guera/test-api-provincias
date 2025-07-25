31d3328af8d8b38518190f8fe8900e00b45a8ad2
Santiago Noguera <santiiago.noguera@gmail.com>
2025-07-25 00:07:25 -0300

    Add view for listar() function

--- resources/views/lista-provincias.blade.php
+++ resources/views/lista-provincias.blade.php
@@ -8,7 +8,26 @@
     <!--Style/Scripts-->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    
+<body class="bg-gray-200">
+    <main class="grid place-items-center">
+        <h1 class="text-3xl font-semibold m-4">Lista de provincias de Argentina</h1>
+        
+        <table class="bg-white rounded mb-4">
+            <thead>
+                <tr>
+                    <th class="p-2">ID</th>
+                    <th class="p-2">Nombre</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($provincias as $provincia)
+                    <tr>
+                        <td class="p-2">{{ $provincia->id }}</td>
+                        <td class="p-2">{{ $provincia->nombre }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </main>
 </body>
 </html>
